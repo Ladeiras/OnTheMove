fa7fe1df60ce6a9064e256dccbe73da05b653e47
João Ladeiras <jtladeiras@gmail.com>
2014-06-02 22:56:50 +0100

    fix

--- App/OnTheMove/onthemove/src/main/java/com/moksie/onthemove/activities/ContactsMenuActivity.java
+++ App/OnTheMove/onthemove/src/main/java/com/moksie/onthemove/activities/ContactsMenuActivity.java
@@ -1,22 +1,43 @@
 package com.moksie.onthemove.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 import com.moksie.onthemove.R;
 import com.moksie.onthemove.fragments.FooterFragment;
+import com.moksie.onthemove.objects.Airport;
 import com.moksie.onthemove.objects.Flight;
 import com.moksie.onthemove.utilities.FileIO;
 
 public class ContactsMenuActivity extends FragmentActivity {
+
+    private Airport airport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contacts);
+        setContentView(R.layout.activity_contacts_menu);
+
+        Intent intent = getIntent();
+        airport = (Airport) intent.getParcelableExtra("airport");
+
+        //Botao posto socorro
+        final Button PSButton = (Button) findViewById(R.id.contacts_posto_socorro);
+        PSButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ContactsMenuActivity.this, ContactActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                intent.putExtra("contact", "postosocorro");
+                intent.putExtra("airport", airport);
+                ContactsMenuActivity.this.startActivity(intent);
+            }
+        });
 
         updateFragments();
     }
